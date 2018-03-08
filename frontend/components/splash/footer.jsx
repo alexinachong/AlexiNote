5b5bdc7f2d53adc7d0bcc0a0cfeb31bb2e14b1ba
Alexina Chong <alexina.chong@gmail.com>
2018-03-08 11:32:13 -0800

    Fix positioning of header and add contact link to footer

--- frontend/components/splash/footer.jsx
+++ frontend/components/splash/footer.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
 class Footer extends React.Component {
-
-  render() {
-    return (
-      <footer className="footer-outer-container">
-        <section className="footer-inner-container">
-          <section className="footer-inner-left">&copy; 2018 Alexina Chong</section>
-          <section className="footer-inner-right">
-            <a href="https://github.com/alexinachong/">GitHub</a> | <a href="https://www.linkedin.com/in/alexinachong/">LinkedIn</a> | <Link to="#">Contact</Link>
-          </section>
-        </section>
-      </footer>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer-outer-container">
+				<section className="footer-inner-container">
+					<section className="footer-inner-left">
+						&copy; 2018 Alexina Chong
+					</section>
+					<section className="footer-inner-right">
+						<a href="https://github.com/alexinachong/">GitHub</a> |{' '}
+						<a href="https://www.linkedin.com/in/alexinachong/">LinkedIn</a> |{' '}
+						<a href="http://www.alexinachong.com/">Contact</a>
+					</section>
+				</section>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
