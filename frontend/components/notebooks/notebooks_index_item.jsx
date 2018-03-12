b37b61824f4bdf1b387e45b6365f73e458b1adab
Alexina Chong <alexina.chong@gmail.com>
2018-03-12 02:15:54 -0700

    Fix size of save note button

--- frontend/components/notebooks/notebooks_index_item.jsx
+++ frontend/components/notebooks/notebooks_index_item.jsx
@@ -3,30 +3,38 @@ import { Link, Redirect } from 'react-router-dom';
 
 // <img onClick={() => deleteNotebook(notebook.id)} src="https://raw.githubusercontent.com/alexinachong/AlexiNote/master/app/assets/images/evernote_delete_trash_icon.png" />
 
-
 const NotebooksIndexItem = ({ notebook, deleteNotebook }) => {
-  let noteCount = "";
-  if (notebook.numNotes === 1) {
-    noteCount = `${notebook.numNotes} note`;
-  } else {
-    noteCount = `${notebook.numNotes} notes`;
-  }
+	let noteCount = '';
+	if (notebook.numNotes === 1) {
+		noteCount = `${notebook.numNotes} note`;
+	} else {
+		noteCount = `${notebook.numNotes} notes`;
+	}
 
-  return (
-    <div>
-      <Link to={`/notebooks/${notebook.id}`}>
-        <div className="notebook-preview">
-          <section className="notebook-preview-header">
-            <p className="notebook-preview-title">{notebook && notebook.title}</p>
-            <p className="notebook-preview-count">{noteCount}</p>
-          </section>
-          <section className="notebook-preview-buttons">
-            <img onClick={() => deleteNotebook(notebook.id).then((response) => this.props.history.push(`/notebooks`))} src="https://raw.githubusercontent.com/alexinachong/AlexiNote/master/app/assets/images/evernote_delete_trash_icon.png" />
-          </section>
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div>
+			<Link to={`/notebooks/${notebook.id}`}>
+				<div className="notebook-preview">
+					<section className="notebook-preview-header">
+						<p className="notebook-preview-title">
+							{notebook && notebook.title}
+						</p>
+						<p className="notebook-preview-count">{noteCount}</p>
+					</section>
+					<section className="notebook-preview-buttons">
+						<img
+							onClick={() =>
+								deleteNotebook(notebook.id).then(response =>
+									this.props.history.push(`/notebooks`)
+								)
+							}
+							src="https://raw.githubusercontent.com/alexinachong/AlexiNote/master/app/assets/images/evernote_delete_trash_icon.png"
+						/>
+					</section>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default NotebooksIndexItem;
